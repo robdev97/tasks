5fb9516b1454e24f0f90f55a46061f3f9f96ddd8
Robert Litwiński <robert.lit97@gmail.com>
2025-03-22 23:09:52 +0100

    Update data

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,8 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,24 +11,26 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/tasks")
 public class TaskController {
-    @RequestMapping(method = RequestMethod.GET, value = "")
+    @GetMapping
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
-    @GetMapping("/{taskId}")
-    public TaskDto getTask(Long taskId) {
+    @GetMapping(value = "{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId) {
         return new TaskDto(1L, "test title", "test_content");
     }
-    @DeleteMapping("/{taskId")
-    public void deleteTask(Long taskId) {
-
+    @DeleteMapping(value = "{taskId}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long taskId) {
+        return ResponseEntity.noContent().build();
     }
-    @PutMapping
-    public TaskDto updateTask(TaskDto taskDto) {
-        return new TaskDto(1L, "test title", "test_content");
+    @PutMapping("{taskId}")
+    public ResponseEntity<TaskDto> updateTask(@PathVariable Long taskId, @RequestBody TaskDto taskDto) {
+        TaskDto updatedTask = new TaskDto(taskId, taskDto.getTitle(), taskDto.getContent());
+        return ResponseEntity.ok(updatedTask);
     }
     @PostMapping
-    public void createTask(TaskDto taskDto) {
-
+    public ResponseEntity<TaskDto> createTask(@RequestBody TaskDto taskDto) {
+        TaskDto createdTask = new TaskDto(1L, taskDto.getTitle(), taskDto.getContent());
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
     }
 }
