44ff461332b7e7ae1abbfa6328082adb88f9ac62
Robert Litwiński <robert.lit97@gmail.com>
2025-03-24 08:50:56 +0100

    Zadanie: wyszukiwanie pojedynczego zadania

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,11 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,27 +15,16 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
     @GetMapping
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
-    }
-    @GetMapping(value = "{taskId}")
-    public TaskDto getTask(@PathVariable Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
-    }
-    @DeleteMapping(value = "{taskId}")
-    public ResponseEntity<Void> deleteTask(@PathVariable Long taskId) {
-        return ResponseEntity.noContent().build();
-    }
-    @PutMapping("{taskId}")
-    public ResponseEntity<TaskDto> updateTask(@PathVariable Long taskId, @RequestBody TaskDto taskDto) {
-        TaskDto updatedTask = new TaskDto(taskId, taskDto.getTitle(), taskDto.getContent());
-        return ResponseEntity.ok(updatedTask);
-    }
-    @PostMapping
-    public ResponseEntity<TaskDto> createTask(@RequestBody TaskDto taskDto) {
-        TaskDto createdTask = new TaskDto(1L, taskDto.getTitle(), taskDto.getContent());
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
+
     }
 }
---
+++ src/main/java/com/crud/tasks/mapper/TaskMapper.java
@@ -0,0 +1,25 @@
+package com.crud.tasks.mapper;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskMapper {
+
+    public TaskDto mapToTaskDto(final Task task) {
+        return new TaskDto(
+                task.getId(),
+                task.getTitle(),
+                task.getContent()
+        );
+    }
+
+    public List<TaskDto> mapToTaskDtoList(final List<Task> taskList) {
+        return taskList.stream()
+                .map(this::mapToTaskDto)
+                .toList();
+    }
+}
