c39d8f055653e444a48675416fac04c56c6b983e
Robert Litwiński <robert.lit97@gmail.com>
2025-03-24 09:03:40 +0100

    update

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -9,5 +9,6 @@ import java.util.List;
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
     List<Task> findAll();
+    Task save(Task task);
 
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +18,11 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTaskById(Long id) {
-        return repository.findById(id).orElseThrow(() ->
-                new RuntimeException("Task with id " + id + " not found"));
+    public Optional<Task> getTask(final Long taskId) {
+        return repository.findById(taskId);
+    }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
     }
 }
