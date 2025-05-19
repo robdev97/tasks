f2a8252d12cf825bb7b71ec43bc5f091159d8763
Robert Litwiński <robert.lit97@gmail.com>
2025-05-19 14:56:46 +0200

    27.4 Zadanie: rozbudowa endpointa

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -18,7 +18,7 @@ public class EmailScheduler {
     private final AdminConfig adminConfig;
     private static final String SUBJECT = "Tasks: Once a day email";
 
-    @Scheduled(fixedDelay = 10000)
+    @Scheduled(cron = "0 0 10 * * * ")
     public void sendInformationEmail() {
         long size = taskRepository.count();
 
